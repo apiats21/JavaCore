fdbf57b1fa93d4eaccde08d3cfd566cd53c71086
apiats21 <pi134@hotmail.com>
2021-06-21 11:02:17 +0300

    Setting up project..

---
+++ src/main/java/com/apiats21/javacore/chapter03/Main.java
@@ -0,0 +1,8 @@
+package main.java.com.apiats21.javacore.chapter03;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
---
+++ src/main/java/com/apiats21/javacore/chapter04/Class03.java
@@ -0,0 +1,7 @@
+package main.java.com.apiats21.javacore.chapter04;
+
+public class Class03 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
